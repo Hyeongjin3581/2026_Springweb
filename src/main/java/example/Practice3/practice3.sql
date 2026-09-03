1bace8725f17ac3acba70b1cb3cb6faa2c185e72
Hyeongjin3581 <poll3581@naver.com>
2026-09-03 17:57:16 +0900

    practice3번 사전준비

---
+++ src/main/java/example/Practice3/practice3.sql
@@ -0,0 +1,3 @@
+-- Active: 1785826856181@@127.0.0.1@3306@practice3
+-- drop DATABASE  if EXISTS practice3;
+CREATE DATABASE practice3;
--- src/main/java/example/day05/TestService.java
+++ src/main/java/example/day05/TestService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import example.Practice1.test;
+
 import jakarta.transaction.Transactional;
 
 @Service
