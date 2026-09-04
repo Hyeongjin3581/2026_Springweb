fe8af0db20db3550bfe7836b28812d9a3b97b49e
Hyeongjin3581 <poll3581@naver.com>
2026-09-04 18:25:13 +0900

    EqumentDto 생성시작

--- src/main/java/example/minipro2/Dto/EquimentDto.java
+++ src/main/java/example/minipro2/Dto/EquimentDto.java
@@ -1,5 +1,18 @@
 package example.minipro2.Dto;
 
-public class EquimentDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor @AllArgsConstructor 
+@Getter @Setter @ToString @Builder 
+public class EquimentDto {
+    private Integer e_no;
+    private String e_name;
+    private String c_category;
+    private String e_status;
+    private Integer l_no;
 }
