607b51fa84ded345c95831e5b332c22ffba1b609
Hyeongjin3581 <poll3581@naver.com>
2026-09-18 10:21:14 +0900

    123

--- src/main/java/example/day03/Exam2.java
+++ src/main/java/example/day03/Exam2.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
